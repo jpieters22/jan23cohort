5b20bb5619bdf9651fbc7254b6f4bb1cc6736e50
jpieters22 <joslynsipod@gmail.com>
2023-03-20 11:51:20 -0500

    upload

---
+++ assignments/java/FirstJava.java
@@ -0,0 +1,13 @@
+package assignments.java;
+
+public class FirstJava {
+    public static void main(String[] args){
+        String name = "Joslyn";
+        int age = 21;
+        String hometown = "Dallas, TX";
+
+        System.out.println("My name is " + name);
+        System.out.println("My age is " + age);
+        System.out.println("My hometown is " + hometown);
+    }
+}
